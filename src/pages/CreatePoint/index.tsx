247617fd1b01a9b98dbd88a623cf42b1be39ebd2
jbqneto <jbqneto@gmail.com>
2020-06-05 23:12:18 -0300

    create point and map added

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,7 +1,103 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
+
+import './createpoint.css';
+import logo from '../../assets/logo.svg';
 
 const CreatePoint = () => {
-  return <h1>Point</h1>
+  return (
+    <div id="page-create-point">
+      <header>
+        <img src={logo} alt="Ecoleta" className="logo" />
+        <Link to="/">
+          <FiArrowLeft>
+            Voltar para a home
+          </FiArrowLeft>
+        </Link>
+      </header>
+      <main>
+        <form>
+          <h1>Cadastro do <br />ponto de coleta</h1>
+
+          <fieldset>
+            <legend>
+              <h2>Dados</h2>
+            </legend>
+
+            <div className="field">
+              <label htmlFor="name">Nome da entidade</label>
+              <input type="text" name="name" id="name" />
+            </div>
+
+            <div className="field-group">
+              <div className="field">
+                <label htmlFor="email">E-mail</label>
+                <input type="email" name="email" id="email" />
+              </div>
+
+              <div className="field">
+                <label htmlFor="whatsapp">WhatsApp</label>
+                <input type="text" name="whatsapp" id="whatsapp" />
+              </div>
+            </div>
+
+          </fieldset>
+
+          <fieldset>
+            <legend>
+              <h2>Endereço</h2>
+              <span>Selecione o endereço no mapa</span>
+            </legend>
+
+            <div className="map">
+              <Map center={[-27.2092052, -49.6401092]} zoom={15}>
+                <TileLayer
+                  attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[-27.2092052, -49.6401092]}></Marker>
+              </Map>
+            </div>
+
+            <div className="field-group">
+              <div className="field">
+                <label htmlFor="uf">Estado (UF)</label>
+                <select name="uf" id="uf">
+                  <option value="0">Selecione uma UF</option>
+                </select>
+              </div>
+
+              <div className="field">
+                <label htmlFor="uf">Cidade</label>
+                <select name="city" id="city">
+                  <option value="0">Selecione uma cidade</option>
+                </select>
+              </div>
+
+            </div>
+
+          </fieldset>
+
+          <fieldset>
+            <legend>
+              <h2>Ítens de coleta</h2>
+              <span>Selecione um ou mais itens abaixo</span>
+            </legend>
+
+            <ul className="items-grid">
+              <li></li>
+            </ul>
+
+          </fieldset>
+          <button type="submit">
+            Cadastrar
+           </button>
+        </form>
+      </main>
+    </div >
+  )
 };
 
 export default CreatePoint;
