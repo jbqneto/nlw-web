278597c0776cf594121c5fa7a70ebd82ec827066
jbqneto <jbqneto@gmail.com>
2020-06-06 23:23:33 -0300

    finished createPoint

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState, ChangeEvent } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import { LeafletMouseEvent } from 'leaflet';
@@ -38,6 +38,8 @@ const CreatePoint = () => {
     email: '',
     whatsapp: ''
   });
+
+  const history = useHistory();
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(position => {
@@ -114,6 +116,38 @@ const CreatePoint = () => {
     setFormData({ ...formData, [name]: value });
   }
 
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    const { name, email, whatsapp } = formData;
+    const uf = selectedUf;
+    const city = selectedCity;
+    const [latitude, longitude] = selectedPosition;
+    const items = selectedItems;
+
+    const data = {
+      name,
+      email,
+      whatsapp,
+      uf,
+      city,
+      latitude,
+      longitude,
+      items
+    }
+
+    console.log(data);
+
+    api.post('points', data).then((response) => {
+      console.log(response.data);
+      alert('Ponto de coleta criado com sucesso!');
+      history.push('/');
+    }).catch((error) => {
+      console.error(error);
+    });
+
+  }
+
   return (
     <div id="page-create-point">
       <header>
@@ -125,7 +159,7 @@ const CreatePoint = () => {
         </Link>
       </header>
       <main>
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Cadastro do <br />ponto de coleta</h1>
 
           <fieldset>
